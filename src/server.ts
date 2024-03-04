cad297ac9cb8d65ebb26a2af33a2d6916f15cd83
Ismael Guajardo <ish.gdo@gmail.com>
2024-03-03 23:07:30 -0700

    Modify typescript build

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import Koa from 'koa';
-import KoaRouter from '@koa/router';
+import Router from '@koa/router';
 import 'dotenv/config';
 
 const app: Koa = new Koa();
-const router: KoaRouter = new KoaRouter();
+const router: Router = new Router();
 const port: number = 3000;
 
 router.get('/', (ctx: Koa.Context) => {ctx.body = 'Hello Test'});
