2e1b07b8f04cb286f008aad44c3051c32eb82766
Ismael Guajardo <ish.gdo@gmail.com>
2024-03-09 18:52:54 -0700

    Add create user_connections table migration

---
+++ src/entity/UserConnection.ts
@@ -0,0 +1,56 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    OneToOne,
+    JoinColumn,
+    Index
+} from "typeorm";
+import { User } from './User';
+import { Group } from './Group';
+
+@Entity({ name: "user_connections" })
+@Index(['group_id', 'user_id', 'user_id_invitee'])
+export class UserConnection {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    // Column comment: ID of the user that was the inviter.
+    @OneToOne((type) => User, {onDelete: "CASCADE"})
+    @JoinColumn({ name: 'user_id' })
+    user_id: number;
+
+    // Column comment: ID of the user that was invited.
+    @OneToOne((type) => User, {onDelete: "CASCADE"})
+    @JoinColumn({ name: 'user_id_invitee' })
+    user_id_invitee: number;
+
+    // Column comment: ID of group in which users are connected.
+    @OneToOne((type) => Group)
+    @JoinColumn({ name: 'group_id' })
+    group_id: number;
+
+    @Column({ type: 'varchar', comment: 'Relationship between users. Friend, sibling, parent, etc.' })
+    relationship: string
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)" })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)", onUpdate: "CURRENT_TIMESTAMP(6)" })
+    updated_at: Date;
+
+    // Initialize properties to address TS strict mode.
+    epoch = new Date(0);
+
+    constructor() {
+        this.id = 0;
+        this.user_id = 0;
+        this.user_id_invitee = 0;
+        this.group_id = 0;
+        this.relationship = '';
+        this.created_at = this.epoch;
+        this.updated_at = this.epoch;
+    }
+}
