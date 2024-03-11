2d8e6bda5ce31bc3c9b55bb89b015351d9e53032
Ismael Guajardo <ish.gdo@gmail.com>
2024-03-10 21:04:38 -0600

    Restructure project files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config';
 import Koa from 'koa';
-import { setupRoutes } from './routes';
+import { setupRoutes } from './route/routes';
 
 import "reflect-metadata";
-import { AppDataSource } from "./data-source";
+import { AppDataSource } from "./db/data-source";
 
 const app = new Koa();
 const port: number = Number(process.env.APP_PORT) || 3000;
