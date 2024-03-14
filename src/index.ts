643fb7bcb44fcb1ba2344833f33d259d0555b82c
Ismael Guajardo <ish.gdo@gmail.com>
2024-03-13 23:29:02 -0600

    Import koa http request body dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import 'dotenv/config';
 import Koa from 'koa';
+import koaJson from 'koa-json';
+import koaLogger from 'koa-logger';
+import bodyParser from 'koa-bodyparser';
 import { setupRoutes } from './route/routes';
 
 import "reflect-metadata";
 import { AppDataSource } from "./db/data-source";
 
 const app = new Koa();
+
 const port: number = Number(process.env.APP_PORT) || 3000;
 
 AppDataSource.initialize().then(async () => {
+
+  // Allow parsing request body in routes
+  app.use(koaJson());
+  app.use(koaLogger());
+  app.use(bodyParser());
+
   // Routes
   setupRoutes(app);
 
