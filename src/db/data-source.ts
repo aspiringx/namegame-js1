5359ca9c5daaacc13ba38e6481239d06d73d9204
Ismael Guajardo <ish.gdo@gmail.com>
2024-03-13 23:30:55 -0600

    Fix entity typeorm entity path

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -20,7 +20,7 @@ export const AppDataSource = new DataSource({
     password: password,
     database: database,
     logging: false,
-    entities: [`${__dirname}/**/entity/*.{ts,js}`],
+    entities: [`${__dirname}/../../**/entity/*.{ts,js}`],
     migrations: [`${__dirname}/**/migration/*.{ts,js}`],
     subscribers: [],
 })
