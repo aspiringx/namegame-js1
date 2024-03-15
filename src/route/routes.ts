77877829daa52ade1a7119c3194efe41dcf3c6bb
Ismael Guajardo <ish.gdo@gmail.com>
2024-03-14 23:55:53 -0600

    Add sample call route

--- src/route/routes.ts
+++ src/route/routes.ts
@@ -1,18 +1,19 @@
 import Router from "@koa/router";
 import Koa, { Context, Next } from "koa";
 
+import { UserController } from '@controller/UserController';
+
+const userController = new UserController();
+
 export function setupRoutes(app: Koa): void {
     const router = new Router();
 
-    router.get("/", async (ctx: Context, next: Next) => {
-        ctx.body = "Hello Homepage";
-    });
-
-    router.get("/product", async (ctx: Context, next: Next) => {
-        ctx.body = "Hello Product Page";
+    // Sample Call:
+    // App flow demo: Router -> Controller -> Service -> Repository -> Entity
+    router.post("/greet-user", async (ctx: Context, next: Next) => {
+        return ctx.body = await userController.greetUser(ctx.request.body);
     });
 
     app.use(router.routes());
     app.use(router.allowedMethods());
 }
-
