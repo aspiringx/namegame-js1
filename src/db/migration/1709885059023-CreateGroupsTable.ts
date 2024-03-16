d02d75f888d9b73b850180e5a5456792872e32af
Ismael Guajardo <ish.gdo@gmail.com>
2024-03-16 17:35:43 -0600

    Remove wrong column from entity and migration

--- src/db/migration/1709885059023-CreateGroupsTable.ts
+++ src/db/migration/1709885059023-CreateGroupsTable.ts
@@ -11,7 +11,7 @@ export class CreateGroupsTable1709885059023 implements MigrationInterface {
                 \`name\` varchar(255) NULL COMMENT 'Common or short group name.',
                 \`name_full\` varchar(255) NOT NULL COMMENT 'Formal or long group name.',
                 \`slug\` varchar(255) NOT NULL COMMENT 'URL slug for group. Default to lower-case name no spaces.',
-                \`description\` varchar(255) NULL, \`mobile_phone\` varchar(255) NULL COMMENT 'Group description.',
+                \`description\` varchar(255) NULL COMMENT 'Group description,',
                 \`logo_url\` varchar(255) NULL COMMENT 'Optional logo image URL.',
                 \`is_active\` tinyint NOT NULL COMMENT 'Must be active to be visible.' DEFAULT 1,
                 \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
