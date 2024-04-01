95ecd42ee4c7ef80d333666c20ccf95721924f28
Ismael Guajardo <ish.gdo@gmail.com>
2024-04-01 13:37:31 -0600

    Create response format and error handler global middleware

---
+++ src/app/middleware/UncaughtErrorHandler.ts
@@ -0,0 +1,13 @@
+import { Context, Next } from "koa";
+
+export async function uncaughtErrorHandler(ctx: Context, next: Next) {
+    try {
+        await next();
+    } catch (err: any) {
+        // will only respond with JSON
+        ctx.status = err.statusCode || err.status || 500;
+        ctx.body = {
+            message: 'sql' in err ? err.code : err.message
+        };
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import bodyParser from "koa-bodyparser";
 import { restRoutes } from "@route/rest-routes";
 import { graphqlRoute } from "@route/graphql-route";
 
+import { responseFormatHandler } from "@middleware/ResponseFormatHandler";
+import { uncaughtErrorHandler } from "@middleware/UncaughtErrorHandler";
+
 import "reflect-metadata";
 import { AppDataSource } from "./db/data-source";
 
@@ -20,6 +23,12 @@ AppDataSource.initialize().then(async () => {
     app.use(koaJson());
     app.use(koaLogger());
     app.use(bodyParser());
+
+    // Global JSON format
+    app.use(responseFormatHandler);
+
+    // Global error handler
+    app.use(uncaughtErrorHandler);
 
     // Routes
     restRoutes(app);
