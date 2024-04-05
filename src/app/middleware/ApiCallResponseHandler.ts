279238706c07767e9486ded75aa66f0088fd3bb6
Ismael Guajardo <ish.gdo@gmail.com>
2024-04-04 21:49:34 -0600

    Create response handling abstraction and apply to user routes

---
+++ src/app/middleware/ApiCallResponseHandler.ts
@@ -0,0 +1,113 @@
+import { Context, Next } from "koa";
+
+/**
+ * Utility class for formatting response objects.
+ * Middleware taps into http call's context and update status and responses.
+ */
+class responseFormatHandler {
+    private static messages = {fail: '', success: ''};
+
+    /**
+     * Formats response object based on whether fnReturn payload exists or is null.
+     * @param fnReturn The object returned by a function.
+     * @param ctx The Koa context object.
+     * @returns Formatted response object.
+     */
+    static async fromExists(fnReturn: Object | null, ctx: Context): Promise<Object> {
+
+        if(fnReturn) {
+            return { message: this.messages.success,  data: fnReturn};
+        } else {
+            ctx.status = 404;
+            return { message: this.messages.fail,  data: null};
+        }
+    }
+
+    /**
+     * Formats response object based on whether fnReturn returns a count of affected rows or null.
+     * @param count The number of rows affected or null.
+     * @param ctx The Koa context object.
+     * @returns Formatted response object.
+     */
+    static async fromCount(count: number | null, ctx: Context): Promise<Object> {
+        if(count == null) {
+            ctx.status = 404;
+            return { message: this.messages.fail};
+        }
+
+        if(count) {
+            return { message: this.messages.success};
+        } else {
+            return { message: this.messages.fail};
+        }
+    }
+
+    /**
+     * Formats response object for find operation.
+     * @param fnReturn The object returned by an update function.
+     * @param ctx The Koa context object.
+     * @returns Formatted response object.
+     */
+    static async findResource(fnReturn: Object | null, ctx: Context): Promise<Object> {
+        this.messages = {
+            success: 'Resource found.',
+            fail: 'Resource not found.'
+        }
+
+        return this.fromExists(fnReturn, ctx)
+    }
+
+    /**
+     * Formats response object for create operation.
+     * @param fnReturn The object returned by an update function.
+     * @param ctx The Koa context object.
+     * @returns Formatted response object.
+     */
+    static async createResource(fnReturn: Object | null, ctx: Context): Promise<Object> {
+        this.messages = {
+            success: 'Resource creation successful.',
+            fail: 'Resource creation failed.'
+        }
+
+        return this.fromExists(fnReturn, ctx)
+    }
+
+    /**
+     * Formats response object for update operation.
+     * @param fnReturn The object returned by an update function.
+     * @param ctx The Koa context object.
+     * @returns Formatted response object.
+     */
+    static async updateResource(fnReturn: Object | null, ctx: Context): Promise<Object> {
+        this.messages = {
+            success: 'Resource update successful.',
+            fail: 'Resource update failed.'
+        }
+
+        return this.fromExists(fnReturn, ctx)
+    }
+
+    /**
+     * Formats response object for cancel operation.
+     * @param fnReturn The object returned by an update function.
+     * @param ctx The Koa context object.
+     * @returns Formatted response object.
+     */
+    static async deleteResource(fnReturn: Object, ctx: Context): Promise<Object>{
+        let count: number | null = null;
+
+        if('affected' in fnReturn) {
+            count = fnReturn.affected as number | null;
+        }
+        // count = null;
+        this.messages = {
+            success: 'Resource deletion successful.',
+            fail: count === null ? 'Resource deletion failed.' : 'No resource found to delete.'
+        }
+
+        return this.fromCount(count, ctx);
+    }
+
+}
+
+export default responseFormatHandler;
--- src/route/rest-routes.ts
+++ src/route/rest-routes.ts
@@ -1,5 +1,5 @@
 import Router from "@koa/router";
-import Koa, { Context } from "koa";
+import Koa, { Context, Next } from "koa";
 
 import { UserController } from '@controller/UserController';
 import { GroupController } from '@controller/GroupController';
@@ -10,83 +10,83 @@ export function restRoutes(app: Koa): void {
 
     // Sample Call (Step 1): POST call to Controller
     // App flow demo: Router -> Controller -> Service -> Repository -> Entity
-    router.post('/user/greet', async (ctx: Context) => {
-        return ctx.body = await UserController.greetUser(ctx.request.body);
+    router.post('/user/greet', async (ctx: Context, next: Next) => {
+        return ctx.body = await UserController.greetUser(ctx.request.body, ctx);
     });
 
     /* --- User routes --- */
 
     // Get user by id
-    router.get('/user/:userId', async (ctx: Context) => {
-        return ctx.body = await UserController.getUser(ctx.params.userId);
+    router.get('/user/:id', async (ctx: Context, next: Next) => {
+        return ctx.body = await UserController.getUser(ctx.params.id, ctx);
     });
 
     // Get all users
-    router.get('/users', async (ctx: Context) => {
-        return ctx.body = await UserController.getAllUsers();
+    router.get('/users', async (ctx: Context, next: Next) => {
+        return ctx.body = await UserController.getAllUsers(ctx);
     });
 
     // Create user
-    router.post('/user/create', async (ctx: Context) => {
-        return ctx.body = await UserController.createUser(ctx.request.body);
+    router.post('/user/create', async (ctx: Context, next: Next) => {
+        return ctx.body = await UserController.createUser(ctx.request.body, ctx);
     });
 
     // Update user
-    router.put('/user/:userId', async (ctx: Context) => {
-        return ctx.body = await UserController.updateUser(ctx.params.userId, ctx.request.body);
+    router.put('/user/:userId', async (ctx: Context, next: Next) => {
+        return ctx.body = await UserController.updateUser(ctx.params.userId, ctx.request.body, ctx);
     });
 
     // Delete user
-    router.delete('/user/:userId', async (ctx: Context) => {
-        return ctx.body = await UserController.deleteUser(ctx.params.userId);
+    router.delete('/user/:userId', async (ctx: Context, next: Next) => {
+        return ctx.body = await UserController.deleteUser(ctx.params.userId, ctx);
     });
 
     /* --- Group routes --- */
 
     // Get group by id
-    router.get('/group/:groupId', async (ctx: Context) => {
+    router.get('/group/:groupId', async (ctx: Context, next: Next) => {
         return ctx.body = await GroupController.getGroup(ctx.params.groupId);
     });
 
     // Get all groups
-    router.get('/groups', async (ctx: Context) => {
+    router.get('/groups', async (ctx: Context, next: Next) => {
         return ctx.body = await GroupController.getAllGroups();
     });
 
     // Create group
-    router.post('/group/create', async (ctx: Context) => {
+    router.post('/group/create', async (ctx: Context, next: Next) => {
         return ctx.body = await GroupController.createGroup(ctx.request.body);
     });
 
     // Update group
-    router.put('/group/:groupId', async (ctx: Context) => {
+    router.put('/group/:groupId', async (ctx: Context, next: Next) => {
         return ctx.body = await GroupController.updateGroup(ctx.params.groupId, ctx.request.body);
     });
 
     // Delete group
-    router.delete('/group/:groupId', async (ctx: Context) => {
+    router.delete('/group/:groupId', async (ctx: Context, next: Next) => {
         return ctx.body = await GroupController.deleteGroup(ctx.params.groupId);
     });
 
     /* --- Group users routes --- */
 
     // Get users in group by group id
-    router.get('/group/:groupId/users', async (ctx: Context) => {
+    router.get('/group/:groupId/users', async (ctx: Context, next: Next) => {
         return ctx.body = await GroupUserController.getGroupUsers(parseInt(ctx.params.groupId));
     });
 
     // Add user to group
-    router.post('/group/:groupId/user', async (ctx: Context) => {
+    router.post('/group/:groupId/user', async (ctx: Context, next: Next) => {
         return ctx.body = await GroupUserController.createGroupUser(ctx.params.groupId, ctx.request.body);
     });
 
     // Update user in group
-    router.put('/group/:groupId/user/:userId', async (ctx: Context) => {
+    router.put('/group/:groupId/user/:userId', async (ctx: Context, next: Next) => {
         return ctx.body = await GroupUserController.updateGroupUser(parseInt(ctx.params.groupId), parseInt(ctx.params.userId), ctx.request.body);
     });
 
     // Delete user in group
-    router.delete('/group/:groupId/user/:userId', async (ctx: Context) => {
+    router.delete('/group/:groupId/user/:userId', async (ctx: Context, next: Next) => {
         return ctx.body = await GroupUserController.deleteGroupUser(ctx.params.groupId, ctx.params.userId);
     });
 
