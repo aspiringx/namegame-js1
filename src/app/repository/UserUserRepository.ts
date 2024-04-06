7205eb7f0ac15609bc492e2f231e1bd2697c013d
Joe Tippetts <joe@fonespark.com>
2024-04-06 11:16:33 -0600

    wip commit. don't want mysql_data dir tracked.

--- src/app/repository/UserUserRepository.ts
+++ src/app/repository/UserUserRepository.ts
@@ -1,7 +1,7 @@
 import { AppDataSource } from "../../db/data-source";
-import { UserUser } from '@entity/UserUser';
+import { UserUserEntity } from '@entity/UserUserEntity';
 
-export const UserUserRepository = AppDataSource.getRepository(UserUser).extend({
+export const UserUserRepository = AppDataSource.getRepository(UserUserEntity).extend({
 
 
 
